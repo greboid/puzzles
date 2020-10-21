f274d0309c2bf7f367a3f473b569ecc832686892
Greg Holmes <git@greg.holmes.name>
2020-10-21 01:14:10 +0100

    Empty input field on flags clear

--- templates/main.js
+++ templates/main.js
@@ -24,6 +24,7 @@ function ready() {
         value.addEventListener('change', e => handleCategoryChange(e))
     })
     document.querySelector("#flagsform span").onclick = function() {
+        flagterms.value = ""
         while (flagresults.firstChild) {
             flagresults.removeChild(flagresults.lastChild)
         }
