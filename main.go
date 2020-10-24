5df16b3df820a192ea5b962bbf7a94abae9e3b46
Greg Holmes <git@greg.holmes.name>
2020-10-24 19:57:55 +0100

    Simplify HTTP handling (ditch templates)

--- main.go
+++ main.go
@@ -3,23 +3,20 @@ package main
 import (
 	"context"
 	"flag"
-	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/csmith/kowalski/v2"
-	"github.com/fsnotify/fsnotify"
 	"github.com/kouhin/envflag"
 )
 
 var (
-	templates         *template.Template
 	wordList          = flag.String("wordlist-dir", "/app/wordlists", "Path of the word list directory")
-	templateDirectory = flag.String("template-dir", "/app/templates", "Path of the templates directory")
 	words             []*kowalski.SpellChecker
 	download          = flag.Bool("download-flags", false, "Download new flags data")
 )
@@ -42,13 +39,8 @@ func main() {
 	}
 	log.Printf("Loading wordlist.")
 	words = loadWords(*wordList)
-	log.Print("Loading templates.")
-	reloadTemplates()
-	templateChanges()
 	mux := http.NewServeMux()
-	mux.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir(filepath.Join(".", "static")))))
-	mux.HandleFunc("/favicon.ico", faviconHandler)
-	mux.HandleFunc("/", indexHandler)
+	mux.Handle("/", disableDirectoryListing(http.FileServer(http.Dir(filepath.Join(".", "static")))))
 	mux.HandleFunc("/anagram", multiplexHandler(kowalski.MultiplexAnagram))
 	mux.HandleFunc("/match", multiplexHandler(kowalski.MultiplexMatch))
 	mux.HandleFunc("/morse", multiplexHandler(kowalski.MultiplexFromMorse))
@@ -73,40 +65,15 @@ func main() {
 	log.Print("Finishing server.")
 }
 
-func templateChanges() {
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		log.Print("Unable to create watcher")
-		return
-	}
-	err = watcher.Add(filepath.Join("./templates"))
-	if err != nil {
-		log.Print("Unable to watch template folder")
-	}
-	go func() { templateReloader(watcher) }()
-}
-
-func templateReloader(watcher *fsnotify.Watcher) {
-	for {
-		select {
-		case _, ok := <-watcher.Events:
-			if !ok {
-				return
-			}
-			reloadTemplates()
-		case err, ok := <-watcher.Errors:
-			if !ok {
-				return
-			}
-			log.Println("error:", err)
+func disableDirectoryListing(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" && strings.HasSuffix(r.URL.Path, "/") {
+			http.NotFound(w, r)
+			return
 		}
-	}
-}
 
-func reloadTemplates() {
-	templates = template.Must(template.ParseFiles(
-		filepath.Join(*templateDirectory, "index.html"),
-	))
+		next.ServeHTTP(w, r)
+	})
 }
 
 func requestLogger(targetMux http.Handler) http.Handler {
@@ -120,23 +87,6 @@ func requestLogger(targetMux http.Handler) http.Handler {
 			r.RequestURI,
 		)
 	})
-}
-
-func indexHandler(writer http.ResponseWriter, request *http.Request) {
-	if request.URL.Path == "/" {
-		err := templates.ExecuteTemplate(writer, "index.html", "")
-		if err != nil {
-			log.Printf("Fucked up: %s", err.Error())
-			writer.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-	} else {
-		writer.WriteHeader(http.StatusNotFound)
-	}
-}
-
-func faviconHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, filepath.Join(".", "static", "favicon.ico"))
 }
 
 func multiplexHandler(function wordsFunction) func(http.ResponseWriter, *http.Request) {
